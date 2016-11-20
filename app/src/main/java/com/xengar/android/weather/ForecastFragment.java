d1860fb5357953d7287a950b154c73390c568aea
Angel Newton <xanewton@gmail.com>
2016-11-20 15:55:25 -0600

    Add log to see fetch resutl

--- app/src/main/java/com/xengar/android/weather/ForecastFragment.java
+++ app/src/main/java/com/xengar/android/weather/ForecastFragment.java
@@ -139,6 +139,7 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast string to JSON: " + forecastJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
