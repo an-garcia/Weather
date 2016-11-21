9bc8657e79410241f38123cd398b6eb848eb9af1
Angel Newton <xanewton@gmail.com>
2016-11-21 15:58:58 -0600

    Change buildWeatherLocation to build the Uri. Add test

--- app/src/androidTest/java/com/xengar/android/weather/FullTestSuite.java
+++ app/src/androidTest/java/com/xengar/android/weather/FullTestSuite.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.xengar.android.weather;
 
 import android.test.suitebuilder.TestSuiteBuilder;
