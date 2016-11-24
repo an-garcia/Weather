827b5f69965b88a349b58f50ba1fe4033a60882b
Angel Newton <xanewton@gmail.com>
2016-11-24 14:16:58 -0600

    Schedule Synchronization

--- app/src/main/java/com/xengar/android/weather/MainActivity.java
+++ app/src/main/java/com/xengar/android/weather/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.xengar.android.weather.sync.WeatherSyncAdapter;
+
 public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        WeatherSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
